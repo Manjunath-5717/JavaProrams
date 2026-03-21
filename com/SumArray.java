b1e5dc8aa17bc669dcec2db5245fc4fddb0d4a62
Manjunath <manjunathragati704@gmail.com>
2026-03-21 22:48:13 +0530

    sum of numbers using array

---
+++ com/SumArray.java
@@ -0,0 +1,14 @@
+public class SumArray {
+
+    public static void main(String[] args) {
+        int [] a={3,4,6,8,8};
+
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            sum=sum+a[i];
+            
+        }
+        System.out.println("sum "+ sum);
+    }
+    
+}
