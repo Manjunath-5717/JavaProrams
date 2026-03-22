1d378491368fb4ca90ad349a8029760447376a31
Manjunath <manjunathragati704@gmail.com>
2026-03-22 22:22:32 +0530

    it is counting the natural numbers using while loop. when we don't know the no of iteration then only we are using while loop

---
+++ com/NaturalCount.java
@@ -0,0 +1,15 @@
+public class NaturalCount {
+
+    public static void main(String[] args) {
+        int n=1234;
+        int count=0;
+        while(n!=0){
+            int digit= n%10;
+            count++;
+            n=n/10;
+
+        }
+        System.out.println(count);
+    }
+    
+}
