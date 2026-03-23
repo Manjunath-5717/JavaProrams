eb0838b3993046def4f9eff8ce335032767dfb3c
Manjunath <manjunathragati704@gmail.com>
2026-03-23 22:37:15 +0530

    dafault constrouctor it is impliciltly created in every class

---
+++ com/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    int id;
+    String name;
+
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.id=20;
+        s.name="manju";
+
+        System.out.println(s.id);
+        System.out.println(s.name);
+    }
+}
