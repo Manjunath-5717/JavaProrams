6cab442462e6d1d94ebe41566f4eeaaa76468401
Manjunath <manjunathragati704@gmail.com>
2026-03-24 22:09:06 +0530

    i added comment line in this

--- com/Studentt.java
+++ com/Studentt.java
@@ -2,12 +2,11 @@ public class Studentt {
     
     int id;
     String name;
-
+   // parameterized constrouctor
     Studentt(int id, String name){
         this.id=id;
         this.name=name;
     }
-
     public static void main(String[] args) {
      
     Studentt s=new Studentt(101,"manju");
